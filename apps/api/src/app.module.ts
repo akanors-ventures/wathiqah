12f4cc91c2dd6f877ffbe3749d6e3e8718abc1d0
fawazabdganiyu <fawazabdganiyu@gmail.com>
2026-01-21 19:57:24 +0100

    feat: register Users and Auth modules in App module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,10 +7,12 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
@@ -19,8 +21,10 @@ import { ConfigModule } from '@nestjs/config';
     }),
     PrismaModule,
     TransactionsModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
