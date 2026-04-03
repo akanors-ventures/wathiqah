29a6bed6bf85d216a3a5d37eaaec4184c21a42f3
fawazabdganiyu <fawazabdganiyu@gmail.com>
2026-04-03 11:05:04 +0100

    feat(db): add new formal TransactionType enum values; remove Atlas Cloud dependency

---
+++ apps/api/atlas/migrations/20260403100437.sql
@@ -0,0 +1,24 @@
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'LOAN_GIVEN';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'LOAN_RECEIVED';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'REPAYMENT_MADE';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'REPAYMENT_RECEIVED';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'GIFT_GIVEN';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'GIFT_RECEIVED';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'ADVANCE_PAID';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'ADVANCE_RECEIVED';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'DEPOSIT_PAID';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'DEPOSIT_RECEIVED';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'ESCROWED';
+-- Add value to enum type: "TransactionType"
+ALTER TYPE "public"."TransactionType" ADD VALUE 'REMITTED';
